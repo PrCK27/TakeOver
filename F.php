ab1976b1107f508fddcb9f4b63a1d9b1bd65839a
P4TR1CK <51478807+PrCK27@users.noreply.github.com>
2020-12-06 14:47:53 +0700

    Add files via upload

---
+++ F.php
@@ -0,0 +1,167 @@
+<html>
+<body>
+<head>
+	<iframe width="0" height="0" allow="autoplay" src="https://d.top4top.io/m_1563gpmo19.mp3" frameborder="0" allowfullscreen></iframe>
+	 <title>Root@Cry7 Ft P4TR1CK</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=0.75, shrink-to-fit=no">
+    <meta name="theme-color" content="#000">
+    <meta name="description" content="[- Aku Suka Body Mama Mudahh -]">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="icon" type="image/jpg" href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRVf8XiYiuR83kaQ1wxMQqnNDzbyPBeg5xp8Kl4c_mV-UXuEI3o">
+    <link href="https://fonts.googleapis.com/css?family=Almendra+Display|Almendra+SC|Piedra&display=swap" rel="stylesheet">
+            <script type="text/javascript" src="js/bootstrap.min.js"></script>
+			<script type="text/javascript" src="js/jquery.js"></script>
+			<script type="text/javascript" src="js/jquery-1.10.2.min.js.download"></script>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:300,500');
+html, body {
+  width: 100%;
+  height: 100%;
+}
+body {
+  background-image: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/257418/andy-holmes-698828-unsplash.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  min-width: 100vw;
+  font-family: "Roboto Mono", "Liberation Mono", Consolas, monospace;
+  color: rgba(255, 255, 255, .87);
+}
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
+}
+.container, .container > .row, .container > .row > div {
+  height: 100%;
+}
+#countUp {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+#countUp .number {
+  font-size: 4rem;
+  font-weight: 500;
+}
+#countUp .number + .text {
+  margin: 0 0 1rem;
+}
+#countUp .text {
+  font-weight: 300;
+  text-align: center;
+}
+
+<meta name="googlebot" content="noindex">
+<meta name="robots" content="noindex">
+
+</style>
+<style type="text/css">
+<!--
+.sign {
+ text-align: right;
+}
+-->
+</style>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<b><font size="20" face="courier new"><center><SCRIPT>
+farbbibliothek = new Array(); 
+farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100"); 
+farbbibliothek[1] = new Array("#00FF00","#000000","#00FF00","#00FF00"); 
+farbbibliothek[2] = new Array("#00FF00","#FF0000","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00"); 
+farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040"); 
+farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000"); 
+farbbibliothek[5] = new Array("#000000","#000000","#000000","#FFFFFF","#FFFFFF","#FFFFFF"); 
+farbbibliothek[6] = new Array("#0000FF","#FFFF00"); 
+farben = farbbibliothek[4];
+function farbschrift() 
+{ 
+for(var i=0 ; i<Buchstabe.length; i++) 
+{ 
+document.all["a"+i].style.color=farben[i]; 
+} 
+farbverlauf(); 
+} 
+function string2array(text) 
+{ 
+Buchstabe = new Array(); 
+while(farben.length<text.length) 
+{ 
+farben = farben.concat(farben); 
+} 
+k=0; 
+while(k<=text.length) 
+{ 
+Buchstabe[k] = text.charAt(k); 
+k++; 
+} 
+} 
+function divserzeugen() 
+{ 
+for(var i=0 ; i<Buchstabe.length; i++) 
+{ 
+document.write("<span id='a"+i+"' class='a"+i+"'>"+Buchstabe[i] + "</span>"); 
+} 
+farbschrift(); 
+} 
+var a=1; 
+function farbverlauf() 
+{ 
+for(var i=0 ; i<farben.length; i++) 
+{ 
+farben[i-1]=farben[i]; 
+} 
+farben[farben.length-1]=farben[-1]; 
+
+setTimeout("farbschrift()",30); 
+} 
+// 
+var farbsatz=1; 
+function farbtauscher() 
+{ 
+farben = farbbibliothek[farbsatz]; 
+while(farben.length<text.length) 
+{ 
+farben = farben.concat(farben); 
+} 
+farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001)); 
+} 
+setInterval("farbtauscher()",5000); 
+text="Root./Cry7 X P4TR1CK"; //h 
+string2array(text);
+divserzeugen(); 
+//document.write(text);
+</SCRIPT></center></font></b>
+<br>
+<body bgcolor="#00000" text="green">
+<center>
+<center>
+	<div class="text"> Anjai <font color="red">Mantap Slur</font></div>
+	 <h1><font color="green"><div class="text">Hanya Seorang Rendahan Yang Merangkak Naik</div></font></h1>
+            </div>
+        </div>
+    </div>
+</div>            
+ <center>
+ <center>
+ 	<h2><font color="blue"><div class="text">#</font><font color="red">Indonesian</font><font color="white">DefacerRulez_<</div></font></h2>
+
+ <br>We Not Legend But We Just Newbie</br>
+<iframe width="0" height="0" allow="autoplay" src="https://d.top4top.io/m_1563gpmo19.mp3" frameborder="0" allowfullscreen></iframe>
+</html>
+</head>
+</body>
